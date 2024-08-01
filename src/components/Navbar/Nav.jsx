e76e624d418d11fb458dd44f8d5afa4c1e57a9b0
JosephV95 <viltejl@gmail.com>
2024-08-01 01:09:47 -0300

    Se esta editando los links del nav

--- src/components/Navbar/Nav.jsx
+++ src/components/Navbar/Nav.jsx
@@ -8,31 +8,30 @@ import {
 import { useNavigate } from "react-router-dom";
 
 export default function Nav() {
-  
   const [openNav, setOpenNav] = React.useState(false);
   const navigate = useNavigate()
 
-  //? Estado para aplicar estilos a un link activo 
   const [linkActive, setLinkActive] = React.useState("inicio")
+
+  //! funcion para oscurecer el bg del nav al hacer cierta cantidad de scroll hacia abajo 
+  const selectNav = document.getElementById('navMio')
+  const scrolled = ()=>{
+    if (window.scrollY > 100 && linkActive== 'inicio') {
+      selectNav.classList.add('bg-black/70')
+      selectNav.classList.remove('bg-transparent')
+    } else {
+        selectNav.classList.remove('bg-black/70')
+    }
+}
 
   React.useEffect(() => {
     window.addEventListener(
       "resize",
       () => window.innerWidth >= 960 && setOpenNav(false)
     );
-    const selectNav = document.getElementById('navMio')
 
-    //! funcion para oscurecer el bg del nav al hacer cierta cantidad de scroll hacia abajo 
-    const scrolled = ()=>{
-      if (window.scrollY > 100) {
-        selectNav.classList.add('bg-black/70')
-        selectNav.classList.remove('bg-transparent')
-      } else {
-          selectNav.classList.remove('bg-black/70')
-      }
-    }
-    
-     window.addEventListener('scroll', scrolled)
+      selectNav.classList.remove('bg-black')
+      window.addEventListener('scroll', scrolled)
   }, []);
   
   //? Efecto para oscurecer el bg en mobile, se suma al efecto del scroll anterior
@@ -47,22 +46,21 @@ export default function Nav() {
     }
   })
 
-  
   const navList = (
     <ul className="t-2 mb-4 flex flex-col gap-3 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-4 items-center">
       <li>
-        <a href="#inicio" className={`flex items-center  p-1 font-semibold text-base duration-300  ${linkActive == "inicio"? "text-[#ffb03b] underline underline-offset-2" :"text-gray-100"}`} onClick={()=>{setLinkActive("inicio"); navigate("/")}} >
+        <a className={`flex items-center  p-1 font-semibold text-base duration-300 hover:cursor-pointer  ${linkActive == "inicio"? "text-[#ffb03b] underline underline-offset-2" :"text-gray-100"}`} onClick={()=>{setLinkActive("inicio"); navigate("/")}} >
           Inicio
         </a>
       </li>
 
       <li>
-        <a href="#catalogo" className={`flex items-center  p-1 font-semibold text-base duration-300  ${linkActive == "contacto"? "text-[#ffb03b] underline underline-offset-2" :"text-gray-100"}`} onClick={()=>{setLinkActive("contacto"); navigate("/catalogo")}} >
+        <a className={`flex items-center  p-1 font-semibold text-base duration-300 hover:cursor-pointer  ${linkActive == "catalogo"? "text-[#ffb03b] underline underline-offset-2" :"text-gray-100"}`} onClick={()=>{setLinkActive("catalogo"); navigate("/catalogo")}} >
           Catalogo
         </a>
       </li>
       <li>
-        <a href="#contacto" className={`flex items-center  p-1 font-semibold text-base duration-300  ${linkActive == "contacto"? "text-[#ffb03b] underline underline-offset-2" :"text-gray-100"}`} onClick={()=>{setLinkActive("contacto"); navigate("/contacto")}} >
+        <a className={`flex items-center  p-1 font-semibold text-base duration-300 hover:cursor-pointer  ${linkActive == "contacto"? "text-[#ffb03b] underline underline-offset-2" :"text-gray-100"}`} onClick={()=>{setLinkActive("contacto"); navigate("/contacto")}} >
           Contacto
         </a>
       </li>
@@ -70,7 +68,7 @@ export default function Nav() {
   );
 
   return (
-    <Navbar id="navMio" className="fixed top-0 z-[100] h-max max-w-full rounded-b-xl rounded-t-none border-none py-2 px-8 lg:px-28 bg-transparent" 
+    <Navbar id="navMio" className={`fixed top-0 z-[100] h-max max-w-full rounded-b-xl rounded-t-none border-none py-2 px-8 lg:px-28  ${linkActive!="inicio" ?'bg-black' :'bg-transparent' }`  }
     style={{backdropFilter: "none", boxShadow:"none"}}>
       
       <div className="flex items-center justify-between text-gray-200">
@@ -78,7 +76,7 @@ export default function Nav() {
           as="a"
           href="#"
           onClick={()=>{navigate("/")}}
-          className="mr-4 cursor-pointer py-1.5 text-3xl font-bold font-Dancing tracking-widest" //? fuentes agregadas de Google Fonts y se agrego letter spacing
+          className="mr-4 cursor-pointer py-1.5 text-3xl font-bold font-Dancing tracking-widest" 
           data-aos="zoom-in" data-aos-duration="1200" data-aos-easing="linear" data-aos-delay="1800"
         >
           GastronoPage
