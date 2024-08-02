ce070a033934e4cd39b2bdadb413f8ff4a824160
JosephV95 <viltejl@gmail.com>
2024-08-01 23:58:33 -0300

    Se edito contenido

--- src/components/MenuDelResto/MenuDelResto.jsx
+++ src/components/MenuDelResto/MenuDelResto.jsx
@@ -1,6 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
 import ComidaItem from "./ComidaItem";
-// import Filterizr from 'filterizr'
 import { useState } from "react";
 import comidasActions from "../../store/actions/comidasActions";
 import { useNavigate } from "react-router-dom";
@@ -24,31 +23,6 @@ export default function MenuDelResto() {
     }
   }
 
-  //! Array con las categorias de comidas 
-  // const categoriasComidas = ['carnes', 'pastas', 'sopas', 'sushi', 'dulces', 'pizzas', 'hamburguesas', 'ensaladas']
-  
-  // ? Funcion que podria obtener las categorias de un array de comidas
-  // useEffect(() => {
-  //   const categ = []
-  //   comidasRespaldoStore.forEach(comida => {
-  //     if (!categ.includes(comida.categoria)) {
-  //       categ.push(comida.categoria)
-  //       console.log(categ)
-  //     }
-  //   });
-  // },[]);
-
-// ! Configuracion y funciones para el funcionamiento de la libreria Filterizr
-  // useEffect(() => {
-  //     let filterizr = new Filterizr('.filter-container', options);  
-   
-  //   return () => filterizr.destroy();
-  // }, []); // Reinitialize Filterizr when comidasStore changes
-
-  // const options = {
-  //   // Tus opciones de configuración de Filterizr aquí...
-  // };
-
   return (
     <section className="bg-[url('../img/comidas-bg.jpg')] bg-center bg-cover bg-fixed" id="menu">
       <div className="py-16 space-y-5 bg-white/80"> 
@@ -56,8 +30,6 @@ export default function MenuDelResto() {
 
         <div>
           <ul className="flex flex-row flex-wrap justify-center gap-x-3 gap-y-2 ">
-            {/* <li className={`px-4 py-2 cursor-pointer border-2 border-solid border-[#ffb03b] rounded-3xl inline hover:bg-[#ffb03b] hover:text-white duration-300 ${categoriaElegida =="all"? "bg-[#ffb03b] text-white" :""}`} data-filter="all" 
-              onClick={()=> {filtroParaComidas(comidasRespaldoStore, "all")}}>Todas</li> */}
             <li className={`px-4 py-2 cursor-pointer border-2 border-solid border-[#ffb03b] rounded-3xl inline hover:bg-[#ffb03b] hover:text-white duration-300 ${categoriaElegida =="carnes"? "bg-[#ffb03b] text-white" :""}`} data-filter="carnes" 
               onClick={()=> {filtroParaComidas(comidasRespaldoStore, "carnes")}}>Carnes</li>
             <li className={`px-4 py-2 cursor-pointer border-2 border-solid border-[#ffb03b] rounded-3xl inline hover:bg-[#ffb03b] hover:text-white duration-300 ${categoriaElegida =="pastas"? "bg-[#ffb03b] text-white" :""}`} data-filter="pastas" 
@@ -66,13 +38,6 @@ export default function MenuDelResto() {
               onClick={()=> {filtroParaComidas(comidasRespaldoStore, "pizzas")}}>Pizzas</li>
             <li className={`px-4 py-2 cursor-pointer border-2 border-solid border-[#ffb03b] rounded-3xl inline hover:bg-[#ffb03b] hover:text-white duration-300 ${categoriaElegida =="ensaladas"? "bg-[#ffb03b] text-white" :""}`} data-filter="ensaladas" 
               onClick={()=> {filtroParaComidas(comidasRespaldoStore, "ensaladas")}}>Ensaladas</li>
-            
-              {/* {categoriasComidas.map((categoria, index) => (
-                <li key={index} className={`px-4 py-2 cursor-pointer border-2 border-solid border-[#ffb03b] rounded-3xl inline hover:bg-[#ffb03b] hover:text-white duration-300 ${categoriaElegida == categoria? "bg-[#ffb03b] text-white" :""}`} data-filter={categoria} 
-                onClick={()=> {filtroParaComidas(comidasRespaldoStore, categoria)}}>
-                {categoria.charAt(0).toUpperCase() + categoria.slice(1)}</li>
-              ))} */}
-                {/* //! La Funcion de arriba es para lograr que la primer letra del string este en mayuscula */}
           </ul>
         </div>
 
@@ -90,6 +55,5 @@ export default function MenuDelResto() {
         </div>
       </div>
     </section>
-    
   )
 }
