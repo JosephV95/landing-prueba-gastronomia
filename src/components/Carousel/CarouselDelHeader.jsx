53535876cd74062118f20749f04fad13b88b0939
JosephV95 <viltejl@gmail.com>
2024-08-02 19:42:55 -0300

    Se completo funcionamiento del carousel

--- src/components/Carousel/CarouselDelHeader.jsx
+++ src/components/Carousel/CarouselDelHeader.jsx
@@ -21,16 +21,16 @@ export default function CarouselDelHeader() {
       )}
     >
       <div className="h-full w-full  bg-[url('../img/slide/slide-1.jpg')] bg-cover bg-top " id="inicio"> 
-        <div className="h-full w-full absolute bg-black/50 backdrop-brightness-110 flex justify-center">
+        <div className="h-full w-full absolute bg-black/60 backdrop-brightness-110 flex justify-center">
           <CallToAction 
-          tituloEnHTML={<h2 data-aos="fade-down" data-aos-duration="500" data-aos-easing="linear" className="text-2xl sm:text-3xl md:text-4xl font-Kalam font-bold" ><span className="text-orange-500">Delicious</span> Restaurante</h2>}  
+          tituloEnHTML={<h2 data-aos="fade-down" data-aos-duration="500" data-aos-easing="linear" className="text-2xl sm:text-3xl md:text-4xl font-Kalam font-bold" ><span className="text-orange-500">Gastrono</span> Restaurante</h2>}  
           texto={'Descubre por qué somos la elección perfecta: una experiencia gastronómica excepcional, un ambiente acogedor y un servicio de calidad te esperan en nuestro restaurante. Bienvenido a una experiencia única en cada visita'}
           />
         </div>
       </div>
       
       <div className="h-full w-full  bg-[url('../img/slide/slide-2.jpg')] bg-cover bg-top "> 
-        <div className="h-full w-full absolute bg-black/50 backdrop-brightness-110 flex justify-center">
+        <div className="h-full w-full absolute bg-black/60 backdrop-brightness-110 flex justify-center">
           <CallToAction 
           tituloEnHTML={<h2 className="text-2xl sm:text-3xl md:text-4xl font-Kalam font-bold">La mejor comida</h2>}  
           texto={'Deléitate con nuestra exquisita cocina, ambiente acogedor y servicio excepcional. ¡Tu destino culinario perfecto te espera aquí!'}
@@ -38,7 +38,7 @@ export default function CarouselDelHeader() {
         </div>
       </div>
       <div className="h-full w-full  bg-[url('../img/slide/slide-3.jpg')] bg-cover bg-top "> 
-        <div className="h-full w-full absolute bg-black/50 backdrop-brightness-110 flex justify-center">
+        <div className="h-full w-full absolute bg-black/60 backdrop-brightness-110 flex justify-center">
           <CallToAction 
           tituloEnHTML={<h2 className="text-2xl sm:text-3xl md:text-4xl font-Kalam font-bold">Excelente atención</h2>}  
           texto={'Disfruta del ambiente reconfortante y déjate consentir con un servicio que superará tus expectativas. ¡Te esperamos para crear juntos momentos memorables!'}
